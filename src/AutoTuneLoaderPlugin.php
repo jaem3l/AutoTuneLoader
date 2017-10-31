8ef0903ed0c3443ffba76fa63897fecb491566aa
Christopher Hertel <christopher.hertel@sensiolabs.de>
2017-10-31 01:02:22 +0100

    initial commit to kickoff composer plugin using xosofox/codesound to create the auto-tune-loader

---
+++ src/AutoTuneLoaderPlugin.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace jæm3l\AutoTuneLoader;
+
+use Codesound\Mapper;
+use Codesound\Player;
+use Codesound\Sequence;
+use Codesound\SoundDumper;
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+/**
+ * @author Christopher Hertel <christopher.hertel@sensiolabs.de>
+ */
+class AutoTuneLoaderPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function dumpAutoTuneLoader(Event $event): void
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+
+        $values = [];
+        if ($handle = opendir($vendorDir)) {
+            while (false !== ($entry = readdir($handle))) {
+                $file = $vendorDir.DIRECTORY_SEPARATOR.$entry;
+                if (is_dir($file)) {
+                    continue;
+                }
+
+                $index = $this->bignum($file);
+                $length = filesize($file);
+                $values[] = [$index, $length];
+            }
+            closedir($handle);
+        }
+
+        $tuples = (new Mapper())->map($values);
+        $sequence = Sequence::fromTuples($tuples);
+        $player = new Player($sequence);
+
+        (new SoundDumper())->dump($player, __DIR__.'/autotuneloader.ul', __DIR__.'/autotuneloader.wav');
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::POST_AUTOLOAD_DUMP => 'dumpAutoTuneLoader',
+        ];
+    }
+
+    private function bignum(string $filepath): int
+    {
+        return hexdec(substr(sha1($filepath), 0, 15));
+    }
+}
