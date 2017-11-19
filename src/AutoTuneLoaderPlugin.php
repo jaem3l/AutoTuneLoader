5b24c91c35bece33c6a23f08757df48095edb41b
Christopher Hertel <christopher.hertel@sensiolabs.de>
2017-11-19 18:43:49 +0100

    - dropping usage of symfony/finder by using spl iterator - include application sources into sound by composer autoloading config - disable auto-tune-loader if sox is not installed on system

--- src/AutoTuneLoaderPlugin.php
+++ src/AutoTuneLoaderPlugin.php
@@ -13,37 +13,36 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
 
 class AutoTuneLoaderPlugin implements PluginInterface, EventSubscriberInterface
 {
+    private static $activated = true;
+
     public function activate(Composer $composer, IOInterface $io): void
     {
+        if (!$this->isSoxInstalled()) {
+            self::$activated = false;
+            $io->writeError('<error>Please install sox to use auto-tune-loader.</error>');
+        }
     }
 
     public function dumpAutoTuneLoader(Event $event): void
     {
-        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $composer = $event->getComposer();
+        $vendorDir = $composer->getConfig()->get('vendor-dir');
+        $sourceDirs = $this->getSourceDirs($composer->getPackage()->getAutoload());
         $baseDir = dirname($vendorDir);
+        array_push($sourceDirs, $vendorDir);
 
-        $event->getIO()->write('<info>Generating your auto-tune</info>');
-
-        $finder = (new Finder())
-            ->name('*.php')
-            ->in($vendorDir);
+        $event->getIO()->write('<info>Generating your project-tune</info>');
 
         $values = [];
-        /** @var SplFileInfo $file */
-        foreach ($finder as $file) {
-            $path = $file->getRealPath();
-
-            if (false === $path) {
+        foreach ($this->getSourceIterator($sourceDirs) as $sourceFile) {
+            if (!is_readable($sourceFile)) {
                 continue;
             }
-
-            $index = $this->bignum($path);
-            $length = filesize($path);
+            $index = $this->bignum($sourceFile);
+            $length = filesize($sourceFile);
             $values[] = [$index, $length];
         }
 
@@ -52,14 +51,60 @@ class AutoTuneLoaderPlugin implements PluginInterface, EventSubscriberInterface
         $player = new Player($sequence);
 
         (new SoundDumper())->dump($player, $baseDir.'/autotuneloader.ul', $baseDir.'/autotuneloader.wav');
-        unlink($baseDir.'/autotuneloader.ul');
+        @unlink($baseDir.'/autotuneloader.ul');
     }
 
     public static function getSubscribedEvents(): array
     {
+        if (!self::$activated) {
+            return [];
+        }
+
         return [
             ScriptEvents::POST_AUTOLOAD_DUMP => 'dumpAutoTuneLoader',
         ];
+    }
+
+    private function isSoxInstalled(): bool
+    {
+        exec('which sox', $output, $code);
+
+        return 0 === $code;
+    }
+
+    private function getSourceDirs(array $autoload): array
+    {
+        $sourceDirs = [];
+        foreach ($autoload as $type => $mappings) {
+            if (in_array($type, ['files', 'classmap'], true)) {
+                $sourceDirs = array_merge($sourceDirs, array_map(function ($file) {
+                    return realpath(dirname($file));
+                }, $mappings));
+            } else {
+                $sourceDirs = array_merge($sourceDirs, array_map('realpath', array_values($mappings)));
+            }
+        }
+
+        return array_unique($sourceDirs);
+    }
+
+    private function getSourceIterator(array $sourceDirs): \Iterator
+    {
+        $sourceIterator = new \AppendIterator();
+        foreach ($sourceDirs as $sourceDir) {
+            $iterator = new \RecursiveDirectoryIterator(
+                $sourceDir,
+                \RecursiveDirectoryIterator::CURRENT_AS_PATHNAME
+                | \RecursiveDirectoryIterator::SKIP_DOTS
+                | \RecursiveDirectoryIterator::FOLLOW_SYMLINKS
+            );
+
+            $sourceIterator->append(
+                $iterator = new \RecursiveIteratorIterator($iterator, \RecursiveIteratorIterator::SELF_FIRST)
+            );
+        }
+
+        return new \RegexIterator($sourceIterator , '/^.+\.php/i');
     }
 
     private function bignum(string $filepath): int
